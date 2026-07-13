ac071f24f3cb7e96b03810daf5a9768e39af38c7
SnowFlash383935 <snowflash383935@mail.ru>
2026-07-13 18:43:35 +0300

    Create gme_helper.h

---
+++ include/gme_helper.h
@@ -0,0 +1,9 @@
+#ifndef GME_HELPER_H
+#define GME_HELPER_H
+
+#include <stdio.h>
+#include <stdint.h>
+
+int render_gme_to_wav(const char *input_path, const char *output_wav_path, int duration_sec);
+
+#endif
